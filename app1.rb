b396212844367aa6becf203f9bd76256a3e64d6b
Ahmed Badawy <me@ahmed-badawy.com>
2016-03-13 07:12:55 +0200

    commeting init

---
+++ app1.rb
@@ -0,0 +1,29 @@
+
+
+puts 123 #puts outputs with line break
+print 222	# print outputs without line break
+puts 5555
+
+puts "Hello".reverse
+puts "hello".upcase
+puts "HELLO".downcase
+
+VariableName = "Ahmed Badawy"
+puts VariableName
+
+puts 200.next.to_s + " outputs 200 + 1" # .next get the next number // .to_s : to String 
+
+puts 10/3 # outputs only 3 //round result
+puts 10/3.0 #ouputs 3.33333333333 //use 0 to refuse rounded values
+
+puts 10.6.round 
+puts 10.6.floor 
+puts 10.6.ceil 
+
+
+
+
+
+
+
+
